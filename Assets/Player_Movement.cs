744fc88528022b4dbda2cda6fb0714b4e76434a6
sohail-raza <sohail.raza.mail@gmail.com>
2019-11-22 22:05:14 +0000

    Player movement and Camera control
    Created movement and camera control for FPS movement

---
+++ Assets/Player_Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 6.0f;
+    public float gravity = -9.8f;
+    private CharacterController _charCont;
+
+    void Start()
+    {
+        _charCont = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed); // speed limit
+
+        movement.y = gravity;
+
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        _charCont.Move(movement);
+    }
+}
